b69b36c27ebec48b3ba21cfe19cff2aab9410153
sylabtv <sylab.studio@gmail.com>
2026-01-16 13:39:09 +0100

    calcule moyenne

---
+++ exercice-3/functions.js
@@ -0,0 +1,14 @@
+// vos imports ici
+import { numbers } from "./numbers.js";
+let i = 0
+window.addEventListener("DOMContentLoaded", () => {
+   // votre code ici
+// calcule de la moyenne 
+for(i<length++; i++){
+   i=i+i
+}
+
+console.log(numbers);
+export let computeAverage(numbers) = average
+});
+
---
+++ exercice-3/index.js
@@ -0,0 +1,7 @@
+// vos imports ici
+import { functions } from "./functions.js";
+window.addEventListener("DOMContentLoaded", () => {
+   // votre code ici
+console.log(numbers);
+});
+
